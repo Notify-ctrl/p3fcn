96df306d34114022266787e0b18a3c138dd3cc97
notify <notify-ctrl@qq.com>
2022-11-09 17:24:52 +0800

    write script and update CI

--- CVM/checkfmt.py
+++ CVM/checkfmt.py
@@ -6,13 +6,14 @@
 import os
 import re
 
+err = False
+
 # This is to get the directory that the program
 # is currently running in.
 dir_path = os.path.dirname(os.path.realpath(__file__)) + "/DATA"
 
 for root, dirs, files in os.walk(dir_path):
     ignore0a = True
-    punc = ["。", "?", "？", "!", "！", ",", "，", "…", "⋯"]
     for file in files:
         # change the extension from '.mp3' to
         # the one of your choice.
@@ -23,55 +24,39 @@ for root, dirs, files in os.walk(dir_path):
             fp = open(f)
             data = fp.readlines()
             fp.close()
-            lineno = -1
+            lineno = 0
             rewrite = False
             while True:
               lineno = lineno + 1
-              if lineno >= len(data):
+              if lineno > len(data):
                 break
-              cnc = data[lineno]
+              cnc = data[lineno-1]
               secs = cnc.split('\t')
               if len(secs) < 6:
-                #print(fname, "\t- Line", lineno, ": 缺漏翻译")
+                # print(fname, ":", lineno, ": 缺漏翻译")
                 pass
               else:
                 orig = secs[4]
                 tr = secs[5]
                 tr = tr.strip()
                 if tr == "":
-                  #print(fname, "\t- Line", lineno, ": 缺漏翻译")
+                #  print(fname, ":", lineno, ": 缺漏翻译")
                   continue
                 reg = "\{[^\}]+\}"
                 list1 = re.findall(reg, orig)
                 list2 = re.findall(reg, tr)
-                l1 = orig[len(orig) - 1]
-                l2 = tr[len(tr) - 1]
-                if not punc.count(l1):
-                  l1 = ""
-                if not punc.count(l2):
-                  l2 = ""
-                if l2 == "！":
-                  l2 = "!"
-                elif l2 == "？":
-                  l2 = "?"
-                elif l2 == "…":
-                  l2 = "⋯"
-                if l1 != l2:
-                  #if l1 == "" or (l1 != "" and l2 != ""):
-                    print(fname, "\t- Line", lineno, ": 标点不一致 \"", l1, "\"和\"", l2, "\"")
-                  #elif l2 == "":
-                    #tr = tr + l1
-                    #secs[5] = tr + "\n"
-                    #print(secs[5])
-                    #data[lineno] = "\t".join(secs)
-                    #rewrite = True
                 if ignore0a:
                   while list1.count("{0A}"):
                     list1.remove("{0A}")
                   while list2.count("{0A}"):
                     list2.remove("{0A}")
                 if set(list1) != set(list2):
-                  print(fname, "\t- Line", lineno, ": 原文和翻译控制符不同", list1, list2)
+                  err = True
+                  print(str(fname) + ":" + str(lineno), ": 原文和翻译控制符不同", list1, list2)
             if rewrite:
               fp = open(f, "w")
               fp.writelines(data)
+
+
+if err:
+    raise ValueError("Check Failed")
---
+++ CVM/checkwidth.py
@@ -0,0 +1,58 @@
+#!/bin/python
+
+import os
+import re
+
+err = False
+
+dir_path = os.path.dirname(os.path.realpath(__file__)) + "/DATA"
+
+for root, dirs, files in os.walk(dir_path):
+  ignore0a = True
+  for file in files:
+    # change the extension from '.mp3' to
+    # the one of your choice.
+    if file.endswith('.txt') or file.endswith('.TXT'):
+      f = root + '/' + str(file)
+      #print(f)
+      fname = f.replace(dir_path, "")
+      fp = open(f)
+      data = fp.readlines()
+      fp.close()
+      lineno = 0
+      while True:
+        lineno = lineno + 1
+        if lineno > len(data):
+          break
+        cnc = data[lineno-1]
+        secs = cnc.split('\t')
+        if len(secs) < 6:
+          pass
+        else:
+          orig = secs[4]
+          tr = secs[5]
+          tr = tr.strip()
+          if tr == "":
+            continue
+
+          tr.removeprefix(";")
+          tr.removeprefix("；")
+
+          reg = "\{[^\}]+\}"
+
+          trlist = tr.split("{0A}")
+          for substr in trlist:
+            extralen = 0
+            relist = re.findall(reg, substr)
+            for subreg in relist:
+              substr = substr.replace(subreg, '')
+              if subreg == '{F1 0A}' or subreg == '{F1 0B}':
+                extralen += 3
+
+            if len(substr) + extralen > 18:
+              err = True
+              print(str(fname) + ":" + str(lineno) + ": 宽度超18字符")
+
+
+if err:
+    raise ValueError("Check Failed")
